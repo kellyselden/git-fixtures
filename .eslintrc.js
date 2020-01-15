3d1cc913f058f5dc57c8c6a7b0721f00fc95e73a
Kelly Selden <kellyselden@gmail.com>
2020-01-15 19:39:41 +0000

    @kellyselden/node-template

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,10 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018
   },
+  env: {
+    es6: true
+  },
   extends: [
     'sane-node'
-  ],
-  env: {
-    es6: true,
-  },
-  rules: {
-  }
+  ]
 };
